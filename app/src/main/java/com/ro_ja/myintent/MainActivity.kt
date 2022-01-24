9b58d8c14541e75489f8fff88259d83d8711f646
Your Name <boyra75.rb@gmail.com>
2022-01-24 10:05:54 +0900

    add intent

--- app/src/main/java/com/ro_ja/myintent/MainActivity.kt
+++ app/src/main/java/com/ro_ja/myintent/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.ro_ja.myintent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textViewActivity: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textViewActivity = findViewById(R.id.text_view_activity)
+        textViewActivity.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/ro_ja/myintent/SecondActivity.kt
+++ app/src/main/java/com/ro_ja/myintent/SecondActivity.kt
@@ -2,10 +2,15 @@ package com.ro_ja.myintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
     }
+
+
 }
