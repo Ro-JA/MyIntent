072137fe437f773d8c4f6f3eb0e6cdde2fcf3283
Your Name <boyra75.rb@gmail.com>
2022-01-25 22:26:51 +0900

    add getStringExtra()

--- app/src/main/java/com/ro_ja/myintent/SecondActivity.kt
+++ app/src/main/java/com/ro_ja/myintent/SecondActivity.kt
@@ -3,9 +3,11 @@ package com.ro_ja.myintent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var editTextSecondActivity: EditText
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +16,12 @@ class SecondActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button_second_activity).setOnClickListener {
             finish()
         }
+        editTextSecondActivity = findViewById(R.id.edit_text_second_activity)
+        val textMainActivity = intent.getStringExtra("tag1")
+        editTextSecondActivity.setText(textMainActivity)
     }
+
+
 
 
 }
